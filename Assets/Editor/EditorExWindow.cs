04e5d1c33a3c4186b6cfc47a827706e1ff7fa7d4
n00978 <n00978@cybird-52863.local>
2015-06-16 10:00:40 +0900

    オブジェクト名クラス名変数名valueを表示するように

--- Assets/Editor/EditorExWindow.cs
+++ Assets/Editor/EditorExWindow.cs
@@ -4,36 +4,43 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Runtime.Serialization.Json;  //json生成用
 
 
 public class EditorExWindow : EditorWindow
 {
-
-
+	
+	
 	[MenuItem("Window/EditorEx")]
 	static void Open()
 	{
 		EditorWindow.GetWindow<EditorExWindow>( "EditorEx" );
 	}
-
+	
 	string textArea = "";
-
+	private Vector2 scrollPosition = Vector2.zero;
+	
 	void OnGUI()
 	{
 		if( GUILayout.Button("Caputure") )
-		 	Find();
+			Find();
+		
+		scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+		
 		GUILayout.Label( "TextArea" );
 		textArea = GUILayout.TextArea( textArea );
-
+		
+		GUILayout.EndScrollView();	// スクロールバー終了
+		
 	}	 
-
+	
 	private void Find (){
 		// TextAreaクリア
 		textArea = "";
-
+		
 		List<GameObject> AllObjects =  new List<GameObject>();
 		List<MonoBehaviour> allMonobehaviourList = new List<MonoBehaviour> ();
-
+		
 		foreach (GameObject obj in UnityEngine.Resources.FindObjectsOfTypeAll(typeof(GameObject)))
 		{
 			// アセットからパスを取得.シーン上に存在するオブジェクトの場合,シーンファイル（.unity）のパスを取得.
@@ -47,25 +54,25 @@ public class EditorExWindow : EditorWindow
 				AllObjects.Add (obj);
 			}
 		}
-
+		
 		allMonobehaviourList.AddRange (GetComponentsInList<MonoBehaviour> (AllObjects));
-
+		
 		foreach(var componentData in allMonobehaviourList){
 			foreach(MemberInfo mi in componentData.GetType().GetMembers(BindingFlags.Public | BindingFlags.Instance |  BindingFlags.DeclaredOnly)){
-
+				
 				FieldInfo field = componentData.GetType().GetField(mi.Name);
 				if(field != null){
-					var  value = field.GetValue(componentData);
-					textArea +=  componentData.gameObject + " " + mi.Name + "=" + value + "\n";
+					var value = field.GetValue(componentData);
+					textArea +=  componentData.gameObject.name + " " + componentData.GetType() + " " + mi.Name + "=" + value + "\n";
 				}
 			}
 		}
 	}
-
+	
 	public static IEnumerable<T> GetComponentsInList<T> (IEnumerable<GameObject> gameObjects ) where T : Component 
 	{
 		var componentList = new List<T> ();
-
+		
 		foreach (var obj in gameObjects) {
 			var components = obj.GetComponents<T> ();
 			if (components != null) {
