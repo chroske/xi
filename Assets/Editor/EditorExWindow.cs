cbc9c43b17c72a289c85b49d2be8274ff39dfacf
n00978 <n00978@cybird-52863.local>
2015-06-26 19:14:15 +0900

    とりあえずシリアライズしてnullだったら無視することに

--- Assets/Editor/EditorExWindow.cs
+++ Assets/Editor/EditorExWindow.cs
@@ -9,15 +9,16 @@ using MiniJSON;
 
 public class EditorExWindow : EditorWindow
 {
+	// 変数
+	string textArea = "";
 	
-	
+	// method
 	[MenuItem("Window/EditorEx")]
 	static void Open()
 	{
 		EditorWindow.GetWindow<EditorExWindow>( "EditorEx" );
 	}
 	
-	string textArea = "";
 	private Vector2 scrollPosition = Vector2.zero;
 	
 	void OnGUI()
@@ -40,6 +41,7 @@ public class EditorExWindow : EditorWindow
 		
 		List<GameObject> AllObjects =  new List<GameObject>();
 		List<MonoBehaviour> allMonobehaviourList = new List<MonoBehaviour> ();
+		Dictionary<string,List<Dictionary<string,Dictionary<string, string>>>> objDic = new Dictionary<string,List<Dictionary<string,Dictionary<string, string>>>>();
 		
 		foreach (GameObject obj in UnityEngine.Resources.FindObjectsOfTypeAll(typeof(GameObject)))
 		{
@@ -57,22 +59,53 @@ public class EditorExWindow : EditorWindow
 		
 		allMonobehaviourList.AddRange (GetComponentsInList<MonoBehaviour> (AllObjects));
 		
+		string beforeObjectName = null;
+		List<Dictionary<string,Dictionary<string, string>>> compList = new List<Dictionary<string,Dictionary<string, string>>>();
+		
 		foreach(var componentData in allMonobehaviourList){
+			
+			Dictionary<string,Dictionary<string, string>> compDic = new Dictionary<string,Dictionary<string, string>>();
+			Dictionary<string, string> valueDic = new Dictionary<string,string>();
+			
 			foreach(MemberInfo mi in componentData.GetType().GetMembers(BindingFlags.Public | BindingFlags.Instance |  BindingFlags.DeclaredOnly)){
-				
 				FieldInfo field = componentData.GetType().GetField(mi.Name);
 				if(field != null){
 					var value = field.GetValue(componentData);
-
+					
 					if(value != null){
 						value = Json.Serialize(value);
+						if(value != null){
+							valueDic.Add(mi.Name.ToString(),value.ToString());
+						}
 					}
-
-					textArea +=  componentData.gameObject.name + " " + componentData.GetType() + " " + mi.Name + "=" + value + "\n";
+					
+					//textArea += componentData.gameObject.name + " " + componentData.GetType() + " " + mi.Name + "=" + value + "\n";
 				}
 			}
+			
+			//コンポーネントを配列に突っ込んでからオブジェクトごとにディクショナリに突っ込む
+			if(componentData.GetType() != null && valueDic != null){
+				compDic.Add(componentData.GetType().ToString(),valueDic);
+				
+				if(beforeObjectName == null || componentData.gameObject.name.ToString() == beforeObjectName){
+					compList.Add(compDic);
+					objDic[componentData.gameObject.name.ToString()] = compList;
+				} else {
+					//初期化
+					compList = new List<Dictionary<string,Dictionary<string, string>>>();
+					compList.Add(compDic);
+					
+					objDic[componentData.gameObject.name.ToString()] = compList;
+				}
+				
+				beforeObjectName = componentData.gameObject.name.ToString();
+			}
 		}
+		
+		textArea += Json.Serialize (objDic);
+		//Debug.Log(Json.Serialize(objDic));
 	}
+	
 	
 	public static IEnumerable<T> GetComponentsInList<T> (IEnumerable<GameObject> gameObjects ) where T : Component 
 	{
